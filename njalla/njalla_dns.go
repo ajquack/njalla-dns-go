fb6c9c9aba7910ac7d1b19cc18844e1177d1150a
Marten Bichel <marten.bichel@isd.de>
2025-04-02 11:54:32 +0200

    code: Defaults for njalla DNS api

---
+++ njalla/njalla_dns.go
@@ -0,0 +1,6 @@
+package client
+
+const APIVersion string = "1.0"
+const Endpoint string = "https://njal.la/api/1/"
+const HTTPMethod string = "POST"
+const UserAgent string = "njalla-dns/" + APIVersion
